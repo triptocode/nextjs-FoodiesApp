177df56e3acf92a21f8c30d83012f64e02cd0a4e
triptocode <3580rio@gmail.com>
2024-01-17 10:57:44 +0900

    module.css

---
+++ components/main-header.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+
+export default function MainHeader(){
+    return <header className= {classes.header}>
+        <Link className= {classes.logo} href="/">
+            <Image src={logoImg} alt="A palte with food on it" priority></Image>
+            {/* <img src={logoImg.src} alt="A palte with food on it"></img> */}
+        </Link>
+        <nav className= {classes.nav}>
+            <ul>
+                <li>
+                    <Link href="/meas">Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href="/community">Foodies Community</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+}
+
