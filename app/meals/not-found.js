0bf2c4e512afba1d5e451550ee8a7cd6ada681d5
triptocode <3580rio@gmail.com>
2024-04-20 05:20:16 +0900

    revalidatePath, useFormStatus, useFormState

---
+++ app/meals/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main className="notFound">
+      <div class="cage"></div>
+      <h1>Meal not found</h1>
+      <p> Unfortunately, we could not found the requested page or meal data ! </p>
+    </main>
+  );
+}
